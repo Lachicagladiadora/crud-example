3356e2f33539002c354d38b299ca742c06e70a3d
Lachicagladiadora <200379@unsaac.edu.pe>
2023-08-07 19:26:05 -0500

    add html form

--- index.js
+++ index.js
@@ -7,32 +7,32 @@ const app = express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+const users =[]
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+// todo: update endpoint users variables
 // get user by id
-app.get('/user/:id', (req, res) => {
+app.get('/users/:id', (req, res) => {
     const x = req.params.id
     // TODO: get user from DB
     res.json({id:x,name:"juan",age:"20",email:"juan2000@gmail.com"})
   })
 
-app.post('/user', (req, res) => {
+app.post('/users', (req, res) => {
     const newUser = req.body.user;
     // TODO: create user on DB
     res.json({id:Math.round(Math.random()*1000),...newUser})
 })
 
-app.put('/user/:id', (req, res) => {
+app.put('/users/:id', (req, res) => {
     const userId = req.params.id
     const updateUser = req.body.user;
     // TODO: update user on DB
     res.json({id:userId,...updateUser})
 })
 
-app.delete('/user/:id', (req, res) => {
+app.delete('/users/:id', (req, res) => {
     const userId = req.params.id
     // TODO: update user on DB
     // {data,state,message}
