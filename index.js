56b92beeae80a2bc4c6372ff71273712ff00ce10
Lachicagladiadora <200379@unsaac.edu.pe>
2023-09-19 22:16:44 -0500

    complete crud example

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors');
 const bodyParser = require('body-parser')
 
 const port = 3000
@@ -6,23 +7,43 @@ const port = 3000
 const app = express()
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-const users =[]
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const users = [
+  {id: '1',name: "juan", age: "20", email: "juan2000@gmail.com"},
+  {id: '2',name: "maria", age: "24", email: "maria2032@gmail.com"},
+  {id: '3', name: 'Harper', age: '18', email: 'Charlotte107@hotmail.com'},
+  {id: '4', name: 'Violet', age: '20', email: 'Claire893@gmail.com'},
+  {id: '5', name: 'Lillian', age: '67', email: 'Kinsley288@gmail.com'},
+  {id: '6', name: 'Adeline', age: '30', email: 'Ava558@yahoo.com'},
+  {id: '7', name: 'Riley', age: '14', email: 'Riley931@outlook.com'},
+  {id: '8', name: 'Stella', age: '19', email: 'Layla754@gmail.com'},
+  {id: '9', name: 'Cora', age: '78', email: 'Valentina240@outlook.com'},
+  {id: '10', name: 'Harper', age: '25', email: 'brooklyn@outlook.com'},
+]
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 // todo: update endpoint users variables
 // get user by id
 app.get('/users/:id', (req, res) => {
-    const x = req.params.id
-    // TODO: get user from DB
-    res.json({id:x,name:"juan",age:"20",email:"juan2000@gmail.com"})
+    const userId = req.params.id
+    const foundUser = users.find((c)=>c.id===userId)
+    res.json(foundUser ?? null)
   })
+
+app.get('/users', (req, res) => {
+  // TODO: get user from DB
+  res.json(users)
+})
 
 app.post('/users', (req, res) => {
     const newUser = req.body.user;
     // TODO: create user on DB
-    res.json({id:Math.round(Math.random()*1000),...newUser})
+    console.log('1', req.body)
+    console.log('2', newUser)
+    res.json({id:`${Math.round(Math.random()*1000)}`,...newUser})
 })
 
 app.put('/users/:id', (req, res) => {
